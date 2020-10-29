9938ff8f5c5b6edf00f51a818caddc5a1a4eb8ff
Eric Larson <eclarson@smu.edu>
2020-10-28 22:50:17 -0500

    Initial Commit

---
+++ SpeechExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeechExample
+//
+//  Created by Eric Larson on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
